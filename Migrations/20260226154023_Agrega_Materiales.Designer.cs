c7b104be1135751a263dacf731935fc2f65f8434
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-26 11:41:58 -0400

    Agrega migracion incluyendo materiales

--- Migrations/20260226154023_Agrega_Materiales.Designer.cs
+++ Migrations/20260226154023_Agrega_Materiales.Designer.cs
@@ -3,6 +3,7 @@ using CleanHome.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanHome.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20260226154023_Agrega_Materiales")]
+    partial class Agrega_Materiales
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
