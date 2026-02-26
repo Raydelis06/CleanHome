74c0b96748fa121953084eac604032f91acd4c97
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-26 11:39:17 -0400

    Agrega el modelo Materiales

---
+++ Models/Materiales.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanHome.Models
+{
+    public class Materiales
+    {
+        [Key]
+        public int MaterialId { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(0,int.MaxValue, ErrorMessage = "El valor no puede ser menor que 0")]
+        public int CantidadDisponible { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Unidad { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(0, int.MaxValue, ErrorMessage = "El valor no puede ser menor que 0")]
+        public double Precio { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public int ProveedorId { get; set; }
+    }
+}
