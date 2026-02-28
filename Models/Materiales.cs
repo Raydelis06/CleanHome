01dabdc793e94285aab2706b3ac2e777114c31f2
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-02-28 19:47:55 -0400

    Mejora requerimientos en materiales

--- Models/Materiales.cs
+++ Models/Materiales.cs
@@ -7,16 +7,17 @@ namespace CleanHome.Models
         [Key]
         public int MaterialId { get; set; }
         [Required(ErrorMessage = "Este campo es obligatorio")]
-        public string Nombre { get; set; }
+        public string Nombre { get; set; } = null!;
         [Required(ErrorMessage = "Este campo es obligatorio")]
         [Range(0,int.MaxValue, ErrorMessage = "El valor no puede ser menor que 0")]
         public int CantidadDisponible { get; set; }
         [Required(ErrorMessage = "Este campo es obligatorio")]
-        public string Unidad { get; set; }
+        public string Unidad { get; set; } = null!;
         [Required(ErrorMessage = "Este campo es obligatorio")]
-        [Range(0, int.MaxValue, ErrorMessage = "El valor no puede ser menor que 0")]
+        [Range(1, int.MaxValue, ErrorMessage = "El valor debe ser mayor que 0")]
         public double Precio { get; set; }
         [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Este campo es obligatorio")]
         public int ProveedorId { get; set; }
         public Estados Estado { get; set; } = Estados.Activo;
     }
