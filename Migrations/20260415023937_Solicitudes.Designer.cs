623e460d702ba559c4fa61af5c4039862c429486
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-04-15 08:30:28 -0400

    Ultimos cambios

--- Migrations/20260415023937_Solicitudes.Designer.cs
+++ Migrations/20260415023937_Solicitudes.Designer.cs
@@ -4,6 +4,7 @@ using CleanHome.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanHome.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20260415023937_Solicitudes")]
+    partial class Solicitudes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
